e740f4917a72d4742144ab82fff0f0e06721dc04
rblancolll <rblancol@miumg.edu.gt>
2022-06-05 12:40:33 -0600

    4 Arrays

--- registros/ArrayDepartamentos.java
+++ registros/ArrayDepartamentos.java
@@ -2,5 +2,131 @@
 package registros;
 
 public class ArrayDepartamentos extends ArrayCantidad{
-   
+   private String[] vectorTransacciones;  
+    private int indiceVector;
+    private String [] Nombredeldepartamento;
+    private String [] Nombredeldepartamento2;
+    private String [] Nombredeldepartamento4;
+    private String [] Nombredeldepartamento6;
+
+public ArrayDepartamentos()
+ {
+    Nombredeldepartamento =new String[100];
+    Nombredeldepartamento2 =new String[100];
+    Nombredeldepartamento4 =new String[100];
+    Nombredeldepartamento6 =new String[100];
+    vectorTransacciones= new String[100];
+    this.indiceVector=0;
+     
+
+}
+
+public void registrarNombredeldepartamento(String tipo ){
+        indiceVector= indiceVector+1;
+               Nombredeldepartamento[indiceVector]= " Nombre del departamento = ┃"+ tipo +"┃ ";
+        
+    }
+ public void Nombredeldepartamento(String Nombredeldepartamento){
+      this.Nombredeldepartamento(Nombredeldepartamento);
+      
+      }
+      
+ public void registrarNombredeldepartamento2(String tipo ){
+        indiceVector= indiceVector+1;
+               Nombredeldepartamento2[indiceVector]= " Nombre del departamento = ┃"+ tipo +"┃ ";
+        
+    }
+ public void Nombredeldepartamento2(String Nombredeldepartamento2){
+      this.Nombredeldepartamento2(Nombredeldepartamento2);
+      
+      }
+ 
+ public void registrarNombredeldepartamento4(String tipo ){
+        indiceVector= indiceVector+1;
+               Nombredeldepartamento4[indiceVector]= " Nombre del departamento = ┃"+ tipo +"┃ ";
+        
+    }
+ public void Nombredeldepartamento4(String Nombredeldepartamento4){
+      this.Nombredeldepartamento4(Nombredeldepartamento4);
+      
+      }
+ 
+ public void registrarNombredeldepartamento6(String tipo ){
+        indiceVector= indiceVector+1;
+               Nombredeldepartamento6[indiceVector]= " Nombre del departamento = ┃"+ tipo +"┃ ";
+        
+    }
+ public void Nombredeldepartamento6(String Nombredeldepartamento6){
+      this.Nombredeldepartamento6(Nombredeldepartamento6);
+      
+      }
+ 
+ public void mostrarNombredeldepartamento(){
+      
+            for (int i=1; i<Nombredeldepartamento.length;i++){
+                if(Nombredeldepartamento[i] != null){
+               
+                    System.out.println(Nombredeldepartamento[i]);
+                   
+                }
+            }
+            System.out.println("");
+            for(String transaccion : Nombredeldepartamento){
+                if(transaccion != null){
+                   
+                }   
+            } 
+        }
+ 
+ public void mostrarNombredeldepartamento2(){
+      
+            for (int i=1; i<Nombredeldepartamento2.length;i++){
+                if(Nombredeldepartamento2[i] != null){
+               
+                    System.out.println(Nombredeldepartamento2[i]);
+                   
+                }
+            }
+            System.out.println("");
+            for(String transaccion : Nombredeldepartamento2){
+                if(transaccion != null){
+                   
+                }   
+            } 
+        }
+ 
+ public void mostrarNombredeldepartamento4(){
+      
+            for (int i=1; i<Nombredeldepartamento4.length;i++){
+                if(Nombredeldepartamento4[i] != null){
+               
+                    System.out.println(Nombredeldepartamento4[i]);
+                   
+                }
+            }
+            System.out.println("");
+            for(String transaccion : Nombredeldepartamento4){
+                if(transaccion != null){
+                   
+                }   
+            } 
+        }
+ 
+  public void mostrarNombredeldepartamento6(){
+      
+            for (int i=1; i<Nombredeldepartamento6.length;i++){
+                if(Nombredeldepartamento6[i] != null){
+               
+                    System.out.println(Nombredeldepartamento6[i]);
+                   
+                }
+            }
+            System.out.println("");
+            for(String transaccion : Nombredeldepartamento6){
+                if(transaccion != null){
+                   
+                }   
+            } 
+        }
+  
 }
--- registros/MenuNo2.java
+++ registros/MenuNo2.java
@@ -3,10 +3,91 @@ package registros;
 
 import java.util.Scanner;  
 public class MenuNo2  {
+ static  Scanner leer=new Scanner (System.in);
+    ArrayDepartamentos registro=new ArrayDepartamentos();
 
-
-}
- 
-
+    
+public void MenuNo2(int opcion){
         
-      
+        do{
+           System.out.print(" Eliga un departamento ");
+           System.out.println();
+           System.out.println("1" + " Jutiapa ");
+           System.out.println("2" + " Petén ");
+           System.out.println("3" + " Quiché ");
+           System.out.println("4" + " Izabal ");
+           System.out.println("5" + " Huehuetenango ");
+           System.out.println("6" + " Escuintla ");
+           System.out.println("7" + " Mostrar los registros ");
+          
+           
+           opcion = leer.nextInt();
+            
+       switch(opcion){
+
+case 1 -> { 
+           System.out.println("➤➤➤➤➤➤➤➤➤➤➤");
+           System.out.println("Ingrese el nombre del departamento");
+           String Nombredeldepartamento=leer.next();
+           registro.registrarNombredeldepartamento(Nombredeldepartamento+" "+"Posicion No1");
+          
+}
+
+case 2 -> { 
+           System.out.println("➤➤➤➤➤➤➤➤➤➤➤");
+           System.out.println("Ingrese el nombre del departamento");
+           String Nombredeldepartamento2=leer.next();
+           registro.registrarNombredeldepartamento2(Nombredeldepartamento2+" "+"Posicion No2");
+}
+
+case 3 -> { 
+           System.out.println("➤➤➤➤➤➤➤➤➤➤➤");
+           System.out.println("Ingrese el nombre del departamento");
+           String Nombredeldepartamento=leer.next();
+           registro.registrarNombredeldepartamento(Nombredeldepartamento+" "+"Posicion No3");
+           
+}
+
+case 4 -> { 
+           System.out.println("➤➤➤➤➤➤➤➤➤➤➤");
+           System.out.println("Ingrese el nombre del departamento");
+           String Nombredeldepartamento4=leer.next();
+           registro.registrarNombredeldepartamento4(Nombredeldepartamento4+" "+"Posicion No4");
+}
+
+case 5 -> { 
+           System.out.println("➤➤➤➤➤➤➤➤➤➤➤");
+           System.out.println("Ingrese el nombre del departamento");
+           String Nombredeldepartamento=leer.next();
+           registro.registrarNombredeldepartamento(Nombredeldepartamento+" "+"Posicion No5");
+           
+}
+
+case 6-> { 
+           System.out.println("➤➤➤➤➤➤➤➤➤➤➤");
+           System.out.println("Ingrese el nombre del departamento");
+           String Nombredeldepartamento6=leer.next();
+           
+           registro.registrarNombredeldepartamento6(Nombredeldepartamento6+" "+"Posicion No6");
+           
+}
+case 7 -> {
+           System.out.println("Mostrar todos los registros ");
+           
+           //registro.mostrarNombredeldepartamento();   
+           registro.mostrarNombredeldepartamento();
+           registro.mostrarNombredeldepartamento2();
+           registro.mostrarNombredeldepartamento4();
+           registro.mostrarNombredeldepartamento6();
+           
+               }
+   
+
+    }
+    
+}while (opcion !=8);
+}
+}
+       
+        
+        
